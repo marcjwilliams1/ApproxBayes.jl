0370d6578e01dc6bae8c6855383b248b14461ac7
marcjwilliams1 <marc.williams.13@ucl.ac.uk>
2016-11-29 21:48:14 +0000

    added abc results type which prints out results

--- src/ABCalg.jl
+++ src/ABCalg.jl
@@ -4,13 +4,11 @@ function runabc(ABCsetup::ABCRejection, targetdata)
   #initalize array of particles
   particles = Array(ParticleRejection, ABCsetup.nparticles)
 
-  parameters = zeros(Float64, ABCsetup.nparticles, ABCsetup.nparams)
+  #parameters = zeros(Float64, ABCsetup.nparticles, ABCsetup.nparams)
 
-  #set particle indicator to 1
-  i = 1
 
-  #keep track of number of iterations
-  its = 0
+  i = 1 #set particle indicator to 1
+  its = 0 #keep track of number of iterations
 
   while (i < (ABCsetup.nparticles + 1)) & (its < ABCsetup.maxiterations)
 
@@ -25,13 +23,13 @@ function runabc(ABCsetup::ABCRejection, targetdata)
     #if simulated data is less than target tolerance accept particle
     if dist < ABCsetup.ϵ
       particles[i] = ParticleRejection(newparams)
-      parameters[i,:] = newparams
+      #parameters[i,:] = newparams
       i +=1
     end
 
 
   end
 
-  return particles, parameters
+  return ABCrejectionresults(particles, its, ABCsetup)
 
 end
--- src/types.jl
+++ src/types.jl
@@ -12,7 +12,7 @@ type ABCRejection <: ABCtype
   maxiterations::Int64
   prior::Prior
 
-  ABCRejection(sim_func::Function, nparams::Int64, ϵ::Float64, prior::Prior,
+  ABCRejection(sim_func::Function, nparams::Int64, ϵ::Float64, prior::Prior;
     maxiterations = 10000,
     constants = [1.0],
     nparticles = 100,
@@ -20,6 +20,32 @@ type ABCRejection <: ABCtype
   new(sim_func, nparams, ϵ, nparticles, constants, maxiterations, prior)
 
 end
+
+type ABCrejectionresults
+
+  parameters::Array{Float64,2}
+  accratio::Float64
+  numsims::Float64
+
+   function ABCrejectionresults(particles, its, ABCsetup)
+
+      parameters = hcat(map(x -> x.params, particles)...)'
+      accratio = ABCsetup.nparticles/its
+
+      for i in 1:size(parameters, 2)
+         println("Parameter $i")
+         println("\t Mean = $(mean(parameters[:,i]))")
+         println("\t stdev = $(std(parameters[:,i]))")
+      end
+
+      println("")
+      println("Number of simulation = $its")
+      println("Acceptance Ratio = $(accratio)")
+
+      new(parameters, accratio, its)
+   end
+end
+
 
 
 type PriorUniform <: Prior
