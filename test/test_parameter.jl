8f7327d4a533a6c62362a41948de7bd8ba42dba0
marcjwilliams1 <marc.williams.13@ucl.ac.uk>
2017-07-04 11:56:03 +0100

    Added parameter inference test, #2

---
+++ test/test_parameter.jl
@@ -0,0 +1,53 @@
+# this file will test that both algorithms correctly infer parameters from a normal distribution
+
+using Distributions
+
+function getnormal(params, constants, targetdata)
+
+  simdata = rand(Normal(params...), 1000)
+
+  ApproxBayes.ksdist(simdata, targetdata), 1
+
+end
+
+srand(1)
+p1 = 2.0
+p2 = 0.4
+targetdata = rand(Normal(p1, p2), 1000)
+
+#setup ABC alogrithm specifications for Rejection algorithm
+setup = ABCRejection(getnormal,
+  2,
+  0.1,
+  Prior([Uniform, Uniform], [[0.0, 20.0], [0.0, 2.0]]);
+  maxiterations = 10^6,
+  )
+# run ABC inference
+resrejection = runabc(setup, targetdata);
+
+# test that mean value of posterior is within 10% of true value
+@test isapprox(mean(resrejection.parameters, 1)[1], p1, rtol = 0.05)
+@test isapprox(mean(resrejection.parameters, 1)[2], p2, rtol = 0.05)
+
+#now run SMC algorithm
+setup = ABCSMC(getnormal,
+  2,
+  0.05,
+  Prior([Uniform, Uniform], [[0, 20], [0, 2.0]]),
+  )
+ressmc = runabc(setup, targetdata, verbose = false);
+
+# test that mean value of posterior is within 10% of true value
+@test isapprox(mean(ressmc.parameters, 1)[1], p1, rtol = 0.05)
+@test isapprox(mean(ressmc.parameters, 1)[2], p2, rtol = 0.05)
+
+
+#test SMC is more efficient than rejection algorithm
+setup = ABCSMC(getnormal,
+  2,
+  0.1,
+  Prior([Uniform, Uniform], [[0, 20], [0, 2.0]]),
+  )
+ressmc = runabc(setup, targetdata, verbose = false);
+
+@test ressmc.accratio/resrejection.accratio > 1.0
